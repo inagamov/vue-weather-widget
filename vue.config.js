fe731f9d04f3a1dcad190111b6dd16bc1efc2c62
inagamov <inagamov.tyoma@gmail.com>
2022-08-14 11:16:01 +0300

    Configure PWA settings

--- vue.config.js
+++ vue.config.js
@@ -3,4 +3,18 @@ const {defineConfig} = require("@vue/cli-service");
 module.exports = defineConfig({
 	publicPath: "/vue-weather-widget",
 	transpileDependencies: true,
+	pwa: {
+		name: "vue-weather-widget",
+		short_name: "vue-weather-widget",
+		display: "standalone",
+		start_url: ".",
+		themeColor: "#000000",
+		msTileColor: "#000000",
+		appleMobileWebAppCapable: "yes",
+		appleMobileWebAppStatusBarStyle: "black",
+
+		workboxOptions: {
+			skipWaiting: true,
+		},
+	},
 });
